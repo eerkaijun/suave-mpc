297e694efc832bfda857a5bbc2b70856c3557c2b
eerkaijun <kaijuneer@gmail.com>
2024-03-16 19:47:36 +0000

    deploy script

--- script/Setup.s.sol
+++ script/Setup.s.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {Store} from "src/Store.sol";
 
 contract SetupScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        vm.broadcast(deployerKey);
+
+        Store store = new Store();
+        console.log("Store deployed at: %s", address(store));
     }
 }
