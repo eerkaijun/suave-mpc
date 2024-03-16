7e9cc5df472a59267f980439eeeb98fdc224998a
eerkaijun <kaijuneer@gmail.com>
2024-03-16 15:50:00 +0000

    smaller number of key shares

--- test/KeyManagement.t.sol
+++ test/KeyManagement.t.sol
@@ -14,18 +14,11 @@ contract KeyManagementTest is Test {
 
     function testSecretReconstruction() public {
         // key shares that are obtained where the secret is 3
-        // only 5 of these 10 shares are needed to reconstruct the secret
-        uint256[] memory keyShares = new uint256[](10);
-        keyShares[0] = 1496900149669299837; 
-        keyShares[1] = 907175982561104487;
-        keyShares[2] = 1146663827923924004;
-        keyShares[3] = 1540291843729751165;
-        keyShares[4] = 127923025887755424;
-        keyShares[5] = 581884235885364765;
-        keyShares[6] = 677751154719795947;
-        keyShares[7] = 1517720319752830308;
-        keyShares[8] = 613080097069731912;
-        keyShares[9] = 1107582718334023353;
+        // only 2 of these 3 shares are needed to reconstruct the secret
+        uint256[] memory keyShares = new uint256[](3);
+        keyShares[0] = 1204783606085636997; 
+        keyShares[1] = 103724202957580040;
+        keyShares[2] = 1308507809043217034;
 
         keyManagement.submitShares(keyShares);
         int256 result = keyManagement.reconstructResult();
